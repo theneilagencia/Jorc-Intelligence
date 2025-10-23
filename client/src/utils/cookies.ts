333263323f36f960730410e62943b59e6564678e
theneilagencia <vinicius.debian@theneil.com.br>
2025-10-22 22:28:28 -0400

    fix: change cookie SameSite from Strict to Lax
    - SameSite=Strict was preventing cookies from being sent
    - SameSite=Lax allows cookies to be sent on same-site requests
    - This fixes the 'No access token found in cookies' error

--- client/src/utils/cookies.ts
+++ client/src/utils/cookies.ts
@@ -2,11 +2,11 @@
 
 export function setCookie(name: string, value: string, days = 7) {
   const expires = new Date(Date.now() + days * 24 * 60 * 60 * 1000).toUTCString();
-  document.cookie = `${name}=${value}; Expires=${expires}; Path=/; Secure; SameSite=Strict`;
+  document.cookie = `${name}=${value}; Expires=${expires}; Path=/; Secure; SameSite=Lax`;
 }
 
 export function deleteCookie(name: string) {
-  document.cookie = `${name}=; Path=/; Max-Age=0; Secure; SameSite=Strict`;
+  document.cookie = `${name}=; Path=/; Max-Age=0; Secure; SameSite=Lax`;
 }
 
 export function getCookie(name: string) {
